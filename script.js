b94b1dfc150deb86047ebb400ac57f449b4fe7de
Shaarumathisekar <92247321+Shaarumathisekar@users.noreply.github.com>
2022-10-27 13:41:40 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const videoCard = document.querySelector('.video-container');
+
+fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyAr7K2AYRP51zLSdm-kDNP92Ga210Uh600")
+    
+.then(res => res.json())
+.then(data => {
+    data.items.forEach(item => {
+        getChannelIcon(item);
+    })
+})
+.catch(err => console.log(err));
+
+const getChannelIcon = (video_data) => {
+    fetch("https://www.googleapis.com/youtube/v3/channels?key=AIzaSyAr7K2AYRP51zLSdm-kDNP92Ga210Uh600")
+    .then(res => res.json())
+    .then(data => {
+        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+}
+
+const makeVideoCard = (data) => {
+    videoCard.innerHTML += `
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+// search bar
+
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+    if(searchInput.value.length){
+        location.href = searchLink + searchInput.value;
+    }
+})
